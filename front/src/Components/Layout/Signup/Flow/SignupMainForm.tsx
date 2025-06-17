37a54a59b7443aa2f84aba75895883133f7b3630
stormyoasis <goprotege@gmail.com>
2025-06-17 03:14:11 -0700

    logs

--- front/src/Components/Layout/Signup/Flow/SignupMainForm.tsx
+++ front/src/Components/Layout/Signup/Flow/SignupMainForm.tsx
@@ -115,11 +115,11 @@ export default class SignupMainForm extends React.Component<SignupMainFormProps>
 
     submitForm = async (event: React.ChangeEvent<HTMLButtonElement>) => {
         event?.preventDefault();
-
+console.log("validate");
         if (!this.isFormValid()) {
             return false;
         }
-
+console.log("postAccountsAttempt");
         const res = await postAccountsAttempt({
             dryRun: true,
             emailOrPhone: this.props.emailOrPhone,
@@ -127,7 +127,7 @@ export default class SignupMainForm extends React.Component<SignupMainFormProps>
             fullName: this.props.fullName,
             password: this.props.password,
         });
-
+console.log(res);
         if (res.statusText === "OK") {
             this.props.changePage(1);
             return true;
