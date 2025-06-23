dbf26518061999b637a116c48bb88cebcdc61789
stormyoasis <goprotege@gmail.com>
2025-06-22 17:32:23 -0700

    Remove defaults.css import

--- front/src/Components/Common/CombinedStyling.ts
+++ front/src/Components/Common/CombinedStyling.ts
@@ -3,14 +3,12 @@ import * as styles1 from "/src/Components/Layout/Login/LoginLayout.module.css";
 import * as styles2 from "/src/Components/Common/Common.module.css";
 import * as styles3 from "/src/Components/Layout/Signup/SignupLayout.module.css";
 import * as styles4 from "/src/Components/Layout/Header.module.css";
-import * as styles5 from "/src/../public/defaults.css";
 
 export default {
    ...styles1, 
    ...styles2,
    ...styles3,
-   ...styles4,
-   ...styles5
+   ...styles4
 }
 
 export const Div = styled.div.attrs<{ 
